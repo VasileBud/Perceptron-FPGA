dcbcba13ec550008b5aef509dfcce58e37d98be9
Vasile Bud <vasilebud9@gmail.com>
2025-11-07 14:01:40 +0200

    add fpu_multiplier

---
+++ Perceptron.srcs/sources_1/new/FPU_multiplier.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/07/2025 01:59:16 PM
+-- Design Name: 
+-- Module Name: FPU_multiplier - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity FPU_multiplier is
+--  Port ( );
+end FPU_multiplier;
+
+architecture Behavioral of FPU_multiplier is
+
+begin
+
+
+end Behavioral;
